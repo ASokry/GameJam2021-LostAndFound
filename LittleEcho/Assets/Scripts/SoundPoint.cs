b3c5f5023a91b6c462e06e93f4b245ace2d561f1
Thomas Zakh <ThomasZakh@users.noreply.github.com>
2021-01-29 17:50:15 -0800

    Started working on sound wave

---
+++ LittleEcho/Assets/Scripts/SoundPoint.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPoint : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask checkMask;
+
+    [SerializeField]
+    private int numberOfPoints; // How many points does this sound wave consist of initially
+
+    [SerializeField]
+    private float duration;
+
+    private bool stopped = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Stop()
+    {
+        stopped = true;
+    }
+
+    public void Tick(float deltaTime)
+    {
+        if (stopped)
+            return;
+
+        MoveForward(deltaTime);
+        CheckForward(deltaTime);
+    }
+
+    void MoveForward(float deltaTime)
+    {
+        transform.position += transform.right * deltaTime * SoundWave.waveSpeed;
+    }
+
+    void CheckForward(float deltaTime)
+    {
+        Debug.DrawRay(transform.position, transform.right * SoundWave.waveSpeed * deltaTime, Color.white, 0.1F);
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right, SoundWave.waveSpeed * deltaTime, checkMask);
+
+        if (hit.collider != null)
+        {
+            Stop();
+
+            Debug.Log("hit");
+            for (int i = 0; i < 5; i++)
+            {
+                Debug.DrawRay(hit.point, Random.insideUnitSphere, Color.blue, Random.value * 0.4F);
+            }
+        }
+    }
+}
---
+++ LittleEcho/Assets/Scripts/SoundWave.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundWave : MonoBehaviour
+{
+    public const float waveSpeed = 4;
+
+    [SerializeField]
+    private int numberOfPoints; // How many points does this sound wave consist of initially
+
+    [SerializeField]
+    private float duration;
+
+    [SerializeField]
+    private SoundPoint prefab;
+
+    [SerializeField]
+    private List<SoundPoint> points;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("InstantiatePoints", 0.5F);
+    }
+
+    void InstantiatePoints()
+    {
+        int num = 100;
+        for (int i = 0; i < num; i++)
+        {
+            SoundPoint point = Instantiate(prefab, transform.position, Quaternion.Euler(0, 0, i * (360F / num)));
+            points.Add(point);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (SoundPoint point in points)
+            point.Tick(Time.deltaTime);
+    }
+}
+;
