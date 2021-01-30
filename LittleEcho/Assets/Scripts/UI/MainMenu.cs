35e5f3665da073151e902777f10f88436c2aadee
Cole Pergerson <cole.pergerson@gmail.com>
2021-01-30 15:48:05 -0800

    main menu is functional

---
+++ LittleEcho/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] GameObject StartMenu;
+    [SerializeField] GameObject SettingMenu;
+    [SerializeField] GameObject Managers;
+
+    [Header("Setting values")]
+    [SerializeField] Slider masterVolume;
+    [SerializeField] Slider musicVolume;
+    [SerializeField] Slider sfxVolume;
+
+    private void Awake()
+    {
+        //assuming this the first class that gets called when the game starts
+        Instantiate(Managers);
+    }
+
+    private void Start()
+    {
+        InitiateMenus();
+        InitiateSettings();
+    }
+
+    private void InitiateMenus()
+    {
+        StartMenu.SetActive(true);
+        SettingMenu.SetActive(false);
+    }    
+
+    private void InitiateSettings()
+    {
+        masterVolume.minValue = -20;
+        masterVolume.maxValue = 0;
+
+        musicVolume.minValue = -20;
+        musicVolume.maxValue = 0;
+
+        sfxVolume.minValue = -20;
+        sfxVolume.maxValue = 0;
+    }
+
+    public void ChangeMasterVolume(float value)
+    {
+       SoundManager.Instance.SetMasterVolume(value);
+    }
+
+    public void ChangeMusicVolume(float value)
+    {
+       SoundManager.Instance.SetMusicVolume(value);
+    }
+
+    public void ChangeSFXVolume(float value)
+    {
+       SoundManager.Instance.SetSFXVolume(value);
+    }
+
+    public void PlayGame()
+    {
+        GameManager.Instance.LoadScene("Level_test_1", GameManager.GameState.Playing);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+}
