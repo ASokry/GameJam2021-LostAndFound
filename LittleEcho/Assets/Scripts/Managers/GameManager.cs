5e1fb6e4c049d03844fef8820c7a4430040feaa3
Cole Pergerson <cole.pergerson@gmail.com>
2021-01-30 11:18:37 -0800

    GameManager can now load scenes

--- LittleEcho/Assets/Scripts/Managers/GameManager.cs
+++ LittleEcho/Assets/Scripts/Managers/GameManager.cs
@@ -1,16 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager>
 {
-    public enum GameState { Playing, Paused, GameOver}
+    public enum GameState { MainMenu, Playing, Paused, GameOver}
+
 
     private GameState _currentGameState = GameState.Playing;
     public GameState CurrentGameState
     {
         get { return _currentGameState; }
     }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void ChangeGameStateOnSceneLoad(string scene)
+    {
+        // change state based on what scene was loaded
+    }
+
+    // will include transitions
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
 
 
 }
