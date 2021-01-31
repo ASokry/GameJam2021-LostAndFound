156c1d6aa48cfc3a95ada44691b5b56ae2fc5f42
Thomas Zakh <ThomasZakh@users.noreply.github.com>
2021-01-30 21:12:24 -0800

    Reformatted code for better style

--- LittleEcho/Assets/Scripts/SoundWave.cs
+++ LittleEcho/Assets/Scripts/SoundWave.cs
@@ -5,67 +5,86 @@ using Vectrosity;
 
 public class SoundWave : MonoBehaviour
 {
-    public const float WAVE_SPEED = 2;
-    public const float ECHO_MULT = 0.5F;
-    public const float MINIMUM_DURATION = 0.5F;
-    public const int MAXIMUM_ITERATION = 3;
+    // Constants
+    public const float WAVE_SPEED = 2; // Speed at which wave expands
+    public const float ECHO_MULT = 0.5F; // Duration multiplier applied to each echo
+    public const int MAXIMUM_ITERATION = 3; // How many recursive echos are allowed
 
-    private int numberOfPoints = 100; // How many points does this sound wave consist of initially
+    // Has the Init() method been called yet?
+    private bool initialized = false;
 
+    // Max number of points that this sound wave consists of
+    private int numberOfPoints = 100;
+
+    [Header("General Settings")]
+    // How long this sound wave lasts
     [SerializeField]
     private float duration = 4;
     private float timeAlive = 0;
 
+    // What layers count as "walls." Should exclude the sound source itself
+    [SerializeField]
+    public LayerMask checkMask;
+
+    // Sound wave template for echos
+    [SerializeField]
+    private SoundWave echoPrefab;
+
+    // Starts at 0. Each echo increases the iteration by 1
     private int iteration;
 
+    [Header("Audio Settings")]
+    // Audio to play when spawned or echod
     [SerializeField]
     private AudioClip normalSound;
     [SerializeField]
     private AudioClip echoSound;
 
-    [SerializeField]
-    private SoundWave echoPrefab;
-
-    [SerializeField]
-    public LayerMask checkMask;
-
-    [SerializeField]
+    // Representation of the sound wave
     private List<SoundPoint> points;
-
-    [SerializeField]
-    private AudioSource source;
-
     private VectorLine line;
 
-    private bool initialized = false;
+    // Cached and used to play sounds
+    private AudioSource source;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Initialize properties
         if (!initialized)
             Init(duration, 0);
+
+        // Play the source now that it has been intialized
         source.Play();
 
+        // Create a VectorLine for rendering
+        line = new VectorLine(name, new List<Vector3>(), 4F);
+
+        // Based on the number of sound points we need, spawn them in
         points = new List<SoundPoint>();
         SpawnPoints();
     }
 
+    /// <summary>
+    /// Performs any setup necessary, given certain properties that are passed in
+    /// </summary>
+    /// <param name="duration"></param>
+    /// <param name="iteration"></param>
     private void Init(float duration, int iteration)
     {
-        initialized = true;
+        initialized = true; // Prevent this from running multiple times (esp in Start())
 
-        Debug.Log(iteration);
+        // Cache components
+        source = GetComponent<AudioSource>();
 
-        line = new VectorLine(name, new List<Vector3>(), 4F);
-        //line.drawTransform.SetParent(transform);
-
+        // Set up properties tied to duration (the number of sound points)
         this.duration = duration;
         numberOfPoints = PointsPerDuration(duration);
 
+        // Set up properties tied to iteration (audio related)
         this.iteration = iteration;
         source.volume *= AudioVolumePerIteration(iteration);
         source.pitch *= AudioPitchPerIteration(iteration);
-
         source.clip = iteration == 0 ? normalSound : echoSound;
     }
 
@@ -74,6 +93,9 @@ public class SoundWave : MonoBehaviour
         Init(duration, 0);
     }
 
+    /// <summary>
+    /// Generates sound points and adds them to the list
+    /// </summary>
     void SpawnPoints()
     {
         for (int i = 0; i < numberOfPoints; i++)
@@ -90,16 +112,15 @@ public class SoundWave : MonoBehaviour
             }
         }
 
+        // Are there any points to iterate through?
         if (points.Count > 0)
         {
-            // Assign echo status to some points
+            // Make some of the points "echo points"
             int numberOfEchos = EchosPerDurationIn(duration);
             int echoPoint = Random.Range(0, points.Count);
 
             HashSet<int> takenPoints = new HashSet<int>();
 
-
-            // Try a percentage of the points
             for (int i = 0; i < numberOfEchos; i++)
             {
                 int counter = 10000;
@@ -126,7 +147,7 @@ public class SoundWave : MonoBehaviour
 
     public void SpawnEcho(SoundPoint pointIn)
     {
-        if (duration * ECHO_MULT >= MINIMUM_DURATION && iteration + 1 <= MAXIMUM_ITERATION)
+        if (iteration + 1 <= MAXIMUM_ITERATION)
             SpawnSoundWave(pointIn.position, duration * ECHO_MULT, iteration + 1);
     }
 
@@ -140,65 +161,69 @@ public class SoundWave : MonoBehaviour
     void Update()
     {
         float deltaTime = Time.deltaTime;
-
         timeAlive += deltaTime;
 
-        if (timeAlive > duration)
+        // Should the soundwave be destroyed
+        if (timeAlive <= duration)
         {
-            End();
+            // Simulate points
+            for (int i = 0; i < points.Count; i++)
+                points[i].Tick(deltaTime);
+
+            // Display points
+            if (points.Count > 1)
+                RenderPoints();
         }
         else
-        {
-            // Update points
-            for (int i = 0; i < points.Count; i++)
-            {
-                points[i].Tick(deltaTime);
-            }
-
-            // Draw edge
-            if (points.Count > 1)
-            {
-                Color color = iteration == 0 ? Color.yellow : iteration == 1 ? Color.red : iteration == 2 ? Color.magenta : Color.blue;
-
-                float alpha = timeAlive / duration;
-                alpha = 1 - alpha * alpha;
-                color = new Color(color.r, color.g, color.b, color.a * alpha);
-
-                line.color = color;
-
-
-                for (int i = 0; i < points.Count; i++)
-                {
-                    SoundPoint pointB = i < points.Count - 1 ? points[i + 1] : points[0];
-
-                    Vector3 dir = -(points[i].position - pointB.position);
-
-                    float arcLength = 2 * Mathf.PI * (timeAlive * WAVE_SPEED) / numberOfPoints;
-                    bool stopped = points[i].stopped || pointB.stopped;
-                    bool shortEnough = dir.magnitude <= arcLength + 0.1F || (stopped && dir.magnitude <= 1);
-
-                    RaycastHit2D hit = Physics2D.Raycast(points[i].position, dir, dir.magnitude, checkMask);
-                    if (shortEnough && hit.collider == null)
-                    {
-                        line.points3[2 * i] = points[i].position;
-                        line.points3[2 * i + 1] = pointB.position;
-
-                        //Debug.DrawRay(points[i].position, dir, color, deltaTime);
-                    }
-                }
-
-                line.Draw();
-            }
-        }
+            Expired();
     }
 
-    private void End()
+    /// <summary>
+    /// Called when the duration of this sound wave expires
+    /// </summary>
+    private void Expired()
     {
         points.Clear();
         VectorLine.Destroy(ref line);
 
         if (source.clip && timeAlive > source.clip.length)
             Destroy(gameObject);
+    }
+
+    /// <summary>
+    /// Draws a vector line through each point
+    /// </summary>
+    private void RenderPoints()
+    {
+        // Fade color over time
+        Color color = iteration == 0 ? Color.yellow : iteration == 1 ? Color.red : iteration == 2 ? Color.magenta : Color.blue;
+
+        float alpha = timeAlive / duration;
+        alpha = 1 - alpha * alpha;
+        color = new Color(color.r, color.g, color.b, color.a * alpha);
+
+        line.color = color;
+
+        // Iterate through all points and connect each one to its adjacent neighbor
+        for (int i = 0; i < points.Count; i++)
+        {
+            SoundPoint pointB = i < points.Count - 1 ? points[i + 1] : points[0];
+
+            Vector3 dir = -(points[i].position - pointB.position);
+
+            float arcLength = 2 * Mathf.PI * (timeAlive * WAVE_SPEED) / numberOfPoints;
+            bool stopped = points[i].stopped || pointB.stopped;
+            bool shortEnough = dir.magnitude <= arcLength + 0.1F || (stopped && dir.magnitude <= 1);
+
+            RaycastHit2D hit = Physics2D.Raycast(points[i].position, dir, dir.magnitude, checkMask);
+            if (shortEnough && hit.collider == null)
+            {
+                line.points3[2 * i] = points[i].position;
+                line.points3[2 * i + 1] = pointB.position;
+            }
+        }
+
+        line.Draw();
     }
 
     private static int PointsPerDuration(float durationIn)
