ad6523ae991480ddae1950a760b9c8a7cfc7c58d
Cole Pergerson <cole.pergerson@gmail.com>
2021-01-31 10:43:01 -0800

    Made changes to bat, working on spikes (BUG)(BROKEN)

---
+++ LittleEcho/Assets/Scripts/TurnOffSpriteOnPlay.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOffSpriteOnPlay : MonoBehaviour
+{
+
+    private void Start()
+    {
+        var spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer) spriteRenderer.enabled = false;
+    }
+
+}
