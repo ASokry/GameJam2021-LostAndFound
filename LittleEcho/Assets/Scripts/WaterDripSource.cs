231375e9e356a45c646b3bddf1e7320e00054492
Cole Pergerson <cole.pergerson@gmail.com>
2021-01-30 22:07:04 -0800

    Imported spike art, started adding sounds to drip

---
+++ LittleEcho/Assets/Scripts/WaterDripSource.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterDripSource : MonoBehaviour
+{
+    [SerializeField] int dripFrequency = 2;
+
+    private void Start()
+    {
+        InvokeRepeating("DripWater", 0, dripFrequency);
+    }
+
+    public void DripWater()
+    {
+        var waterDrop = ObjectPool.SharedInstance.GetPooledObject();
+        if (waterDrop != null)
+        {
+            waterDrop.transform.position = this.transform.position;
+            waterDrop.SetActive(true);
+        }
+    }
+
+
+}
